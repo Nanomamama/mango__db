0501c34bb8e1e42802af1be04db6def42b880b17
sukandapop <sb6640248123@lru.ac.th>
2025-03-28 15:58:10 +0700

    เพิ่ม edit_product

--- admin/add_product.php
+++ admin/add_product.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>สินค้าผลิตภัณฑ์แปรรูป</title>
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -24,84 +24,31 @@
             <div class="col-md-6">
                 <div class="mb-3">
                     <label class="form-label">ชื่อสินค้าผลิตภัณฑ์</label>
-                    <input type="text" class="form-control" name="mango_name" required>
+                    <input type="text" class="form-control" name="product_name" required>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">รายละเอียดสินค้า</label>
-                    <input type="text" class="form-control" name="scientific_name" required>
+                    <input type="text" class="form-control" name="product_description" required>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">หมวดหมู่สินค้า</label>
-                    <input type="text" class="form-control" name="local_name" required>
+                    <input type="text" class="form-control" name="product_category" required>
                 </div>
                
                 <div class="mb-3">
                     <label class="form-label">รูปสินค้า</label>
                     <input type="file" class="form-control" name="product_image" accept="image/*" required>
                 </div>
-                <div class="mb-3">
-                    <label class="form-label">รูปต้นมะม่วง</label>
-                    <input type="file" class="form-control" name="tree_image" accept="image/*" required>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">รูปใบมะม่วง</label>
-                    <input type="file" class="form-control" name="leaf_image" accept="image/*" required>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">รูปดอกมะม่วง</label>
-                    <input type="file" class="form-control" name="leaf_image" accept="image/*" required>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">รูปกลิ่งมะม่วง</label>
-                    <input type="file" class="form-control" name="leaf_image" accept="image/*" required>
-                </div>
             </div>
             <div class="col-md-6">
                 <div class="mb-3">
-                    <label class="form-label">การขยายพันธุ์</label>
-                    <input type="text" class="form-control" name="propagation_method" required>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">ลักษณะดิน</label>
-                    <input type="text" class="form-control" name="soil_characteristics" required>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">ระยะเวลาเพาะปลูก</label>
-                    <input type="text" class="form-control" name="planting_period" required>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">ช่วงฤดูกาลเก็บเกี่ยว</label>
-                    <input type="text" class="form-control" name="harvest_season" required>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">การแปรรูป</label><br>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="processing_methods[]" value="กวน">
-                        <label class="form-check-label">กวน</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="processing_methods[]" value="ดอง">
-                        <label class="form-check-label">ดอง</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="processing_methods[]" value="แช่อิ่ม">
-                        <label class="form-check-label">แช่อิ่ม</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="processing_methods[]" value="นิยมรับประทานสด">
-                        <label class="form-check-label">นิยมรับประทานสด</label>
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">หมวดมะม่วง</label>
-                    <select class="form-select" name="mango_category" required>
-                        <option value="เชิงพาณิชย์">เชิงพาณิชย์</option>
-                        <option value="เชิงอนุรักษ์">เชิงอนุรักษ์</option>
-                        <option value="บริโภคในครัวเรือน">บริโภคในครัวเรือน</option>
-                    </select>
-                </div>
-            </div>
+                    <label class="form-label">ราคา</label>
+                    <input type="text" class="form-control" name="product_price" required>
+                </div>    
+             </div>
+              
         </div>
+</div>
         <button type="submit" class="btn btn-primary">💾 บันทึก</button>
         <a href="index.php" class="btn btn-secondary">🔙 กลับ</a>
 
